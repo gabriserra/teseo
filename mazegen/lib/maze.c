ef05b5b41d504c1c997655121b6a73449d04c9b3
Gabriele Serra <gabriele_serra@hotmail.it>
2018-07-17 10:23:31 +0200

    First working version of Maze Generator
    First working version of Maze Generator

---
+++ mazegen/lib/maze.c
@@ -0,0 +1,214 @@
+#include "maze.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// -----------------------------------------------------
+// PRIVATE METHOD
+// -----------------------------------------------------
+
+/**
+ * Init a node of the graph 
+ * 
+ * [IN]     node*: pointer to the node to be initialized as maze component
+ * [IN]     uint8_t: node position in the maze (x position)
+ * [IN]     uint8_t: node position in the maze (y position)
+ * [IN]     uint8_t: explorable directions from the node towards neighbors
+ * [IN]     block: primitive type of block (wall, free, ..)
+ * [OUT]    void
+ **/
+static void init_node(struct node* n, uint8_t i, uint8_t j, uint8_t dirs, enum block type) {
+    n->x = i;
+    n->y = j;
+    n->dirs = dirs;
+    n->type = type;
+}
+
+/**
+ * Remove wall between start and dest node 
+ * NOTE: start and dest must be adjacent
+ * 
+ * [IN]     maze*: maze structure
+ * [IN]     node*: pointer of first node
+ * [OUT]    node*: pointer of second node
+ **/
+static void remove_wall(struct maze* m, struct node* start, struct node* dest) {
+    uint8_t         x, y;   // x, y coordinate of node between start and dest
+    struct node*    mid;    // pointer of node between start and dest
+    
+    // compute coordinate of node between start and dest
+    x = start->x + (dest->x - start->x) / 2;
+    y = start->y + (dest->y - start->y) / 2;
+
+    // get node pointer and change type
+    mid = m->graph + (x * m->width) + y;
+    mid->type = NONE;     
+}
+
+/**
+ * Return a pointer to neighbor obtained from "start" following "dir" 
+ * 
+ * [IN]     maze*: maze structure
+ * [IN]     node*: pointer of start
+ * [OUT]    uint8_t: direction of path
+ **/
+static struct node* get_neighbor(struct maze* m, struct node* start, uint8_t dir) {
+    struct node* neighbor = NULL;
+
+    switch (dir) {
+        case RIGHT_DIR:
+            if (start->x + 2 < m->height)
+                neighbor = m->graph + ((start->x + 2) * m->width) + start->y;
+            break;
+        case DOWN_DIR:
+            if (start->y + 2 < m->width)
+                neighbor = m->graph + (start->x * m->width) + (start->y + 2);            
+            break;        
+        case LEFT_DIR:
+            if (start->x - 2 >= 0)
+                neighbor = m->graph + ((start->x - 2) * m->width) + start->y;            
+            break;        
+        case UP_DIR:
+            if (start->y - 2 >= 0)
+                neighbor = m->graph + (start->x * m->width) + (start->y - 2);            
+            break;
+        default:
+            neighbor = NULL;        
+    }
+
+    return neighbor;
+}
+
+/**
+ * Link the provided node to a random neighbor (if possible) and return
+ * the pointer of the neighbor
+ * 
+ * [IN]     node*: pointer to node from which start
+ * [OUT]    node*: pointer to neighbor visited
+ **/
+static struct node* link(struct maze* m, struct node* start) {
+	uint8_t         new_dir;    // randomly generated direction
+	struct node*    neighbor;   // pointer to neighbor
+	
+	if (start == NULL) 
+        return NULL;
+	
+	// while there are directions still unexplored
+	while (start->dirs) {
+		new_dir = (1 << (rand() % 4));
+		
+		// if it has already been explored re-try
+		if (new_dir & ~start->dirs) 
+            continue;
+		
+		// mark direction as explored and get neighbor
+		start->dirs &= ~new_dir;		
+		neighbor = get_neighbor(m, start, new_dir);
+
+        // if neighbor do not exists or is an already linked node then abort
+		if (neighbor == NULL || neighbor->parent != NULL) 
+            continue;
+		
+		// make sure that neighbor node is not a wall
+		if (neighbor->type == NONE) {
+			
+			// adopt node and remove wall between them
+			neighbor->parent = start;
+            remove_wall(m, start, neighbor);
+			
+			// return address of the child node
+			return neighbor;
+		}
+	}
+	
+	// if neighbor can't be linked, turn backwards (return parent's address)
+	return start->parent;
+}
+
+// -----------------------------------------------------
+// PUBLIC METHOD
+// -----------------------------------------------------
+
+/**
+ * Init the maze 
+ * 
+ * [IN]     maze*: pointer to the graph to be initialized as maze
+ * [IN]     uint8_t: maze width (number of block)
+ * [IN]     uint8_t: maze height (number of block)
+ * [OUT]    int: -1 in case of low mem availability, 0 in case of success
+ **/
+int init_maze(struct maze* m, uint8_t width, uint8_t height) {
+	int             i, j;       // iterate over the graph
+    struct node*    n;          // temp pointer to nodes
+	
+	// allocate memory for graph
+	m->graph = calloc(width * height, sizeof(struct node));
+	
+	// out of memory
+    if (m->graph == NULL) 
+        return -1;
+		
+    // fill up remaining info
+    m->width = width;
+    m->height = height;
+
+	// setup whole graph
+	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) {
+
+            // iterate over graph
+            n = m->graph + (i * width) + j;            
+
+			if (i*j % 2)
+                init_node(n, i, j, ANY_DIR, NONE);
+			else
+                init_node(n, i, j, NO_DIR, WALL);
+        }
+    }
+
+	return 0;
+}
+
+/**
+ * Explore the graph in order to create the maze
+ * 
+ * [IN]     maze*: pointer to the maze struct
+ * [OUT]    void
+ **/
+void create_maze(struct maze* m) {
+    struct node* start;    // node from which begin to explore graph
+    struct node* neighbor; // node reached at each step of exploration
+
+    // start from upper right node of graph and set itself as father
+	start = m->graph + m->width + 1; 
+	start->parent = start;
+
+    // with this kind of choice, the entire maze will be explored
+	neighbor = start;
+
+    // initialize random seed
+    srand(time(NULL));
+
+    do {
+        neighbor = link(m, neighbor);
+    } while(neighbor != start);
+}
+
+/**
+ * Draw into the terminal screen a visual representation of the maze
+ * 
+ * [IN]     maze*: pointer to the maze struct
+ * [OUT]    void
+ **/
+void draw_maze(struct maze* m) {
+	int i, j;   // iterate over the graph
+
+	for (i = 0; i < m->height; i++) {
+		for (j = 0; j < m->width; j++)
+            if(m->graph[i * m->width + j].type == WALL)
+			    printf("%s", "█");
+            else
+                printf("%s", " ");
+		printf("\n");
+    }
+}
---
+++ mazegen/main.c
@@ -0,0 +1,267 @@
+#include "lib/sdfparser.h"
+#include "lib/maze.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// gcc -o exe test.c lib/sdfparser.c lib/list.c lib/maze.c
+
+
+#define MAX_POSE_LEN    30
+#define MAX_NAME_LEN    20
+
+#define WORLD_FILE      "sdf-element/world.sdf"
+#define BOX_FILE        "sdf-element/box.sdf"       
+
+#define NUM_FILES       4
+#define LIGHT_FILE      "sdf-element/light.sdf"     
+#define GUI_FILE        "sdf-element/gui.sdf"
+#define GROUND_FILE     "sdf-element/ground.sdf"
+#define PHYSICS_FILE    "sdf-element/physics.sdf"
+char* names[NUM_FILES] = {LIGHT_FILE, GUI_FILE, GROUND_FILE, PHYSICS_FILE};
+
+/**
+ * Print the message and return the retval
+ * [IN] message: message to be terminal-printed
+ * [IN] retval: value to be returned to system
+ * [OUT] void
+ **/
+void build_world(struct sdf_document* world_d);
+
+/**
+ * Print the message and return the retval
+ * [IN] message: message to be terminal-printed
+ * [IN] retval: value to be returned to system
+ * [OUT] void
+ **/
+void print_and_die(char* message, int retval);
+
+/**
+ * Search for a tag and replace its content
+ * [IN] struct sdf_element*: pointer to element in which search
+ * [IN] char*: tag name
+ * [IN] char*: new tag content
+ * [OUT] void
+ **/
+void search_n_replace_cont(struct sdf_element* elem, char* tag, char* content);
+
+/**
+ * Search for a tag, its attribute name and substitute its value
+ * [IN] struct sdf_element*: pointer to element in which search
+ * [IN] char*: tag name
+ * [IN] char*: attribute name
+ * [IN] char*: new value
+ * [OUT] void
+ **/
+void search_n_replace_attr(struct sdf_element* elem, char* tag, char* name, char* value);
+
+/**
+ * Build a box and add it to the world
+ * [IN] message: message to be terminal-printed
+ * [IN] retval: value to be returned to system
+ * [OUT] void
+ **/
+void add_box(struct sdf_document* world, int box_id, float x, float y, float z);
+
+/**
+ * Generate a maze and print onto screen
+ * [IN] struct maze*: maze will be stored here
+ * [IN] char* w_str: must contain width in string version
+ * [IN] char* h_str: must contain height in string version
+ * [OUT] void
+ **/
+void generate_maze(struct maze* m, char* w_str, char* h_str);
+
+int main(int argc, char* argv[]) {
+    struct sdf_file world_f;
+    struct sdf_document world_d;
+    struct maze m;    
+    int i, j;
+
+    // usage infos
+    if (argc < 3) {
+        printf("Usage: %s <rows> <colum>\n", argv[0]);
+        exit(-1);
+    }
+	
+    // open world file and parse it
+    sdf_file_open(&world_f, WORLD_FILE);    
+    sdf_document_create(&world_f, &world_d);
+
+    // build the world using basic sdf-elements
+    build_world(&world_d);
+
+    // generate the maze
+    generate_maze(&m, argv[1], argv[2]);
+
+    // for each block of the maze, add a box into the 3D world
+    for (i = 0; i < m.height; i++) {
+		for (j = 0; j < m.width; j++)
+            if(m.graph[i * m.width + j].type == WALL) {
+                add_box(&world_d, i*m.width+j, i * 0.5 , j * 0.5 , 0);
+            }
+    }
+    
+    // export the document to file
+    sdf_document_print(&world_d, "maze.world");
+    
+    // free memory
+    sdf_document_close(&world_d);
+    sdf_file_close(&world_f);
+
+    // everything ok
+    return 0;
+}
+
+/**
+ * Print the message and return the retval
+ * [IN] message: message to be terminal-printed
+ * [IN] retval: value to be returned to system
+ * [OUT] void
+ **/
+void print_and_die(char* message, int retval) {
+    fprintf(stderr, "ERROR: %s \n", message);
+    exit(retval);
+}
+
+/**
+ * Search for a tag and replace its content
+ * [IN] struct sdf_element*: pointer to element in which search
+ * [IN] char*: tag name
+ * [IN] char*: new tag content
+ * [OUT] void
+ **/
+void search_n_replace_cont(struct sdf_element* elem, char* tag, char* content) {
+    struct sdf_element* e;  // will contain the tag found
+	
+    // search the tag
+    e = sdf_element_search(elem, tag);
+
+    // die if not found (fatal error)
+    if(e == NULL)
+        print_and_die("Unable to find request tag.", -1);
+    
+    // replace the string
+    sdf_replace_string(e->content, content);
+}
+
+/**
+ * Search for a tag, its attribute name and substitute its value
+ * [IN] struct sdf_element*: pointer to element in which search
+ * [IN] char*: tag name
+ * [IN] char*: attribute name
+ * [IN] char*: new value
+ * [OUT] void
+ **/
+void search_n_replace_attr(struct sdf_element* elem, char* tag, char* name, char* value) {
+    struct sdf_element*     e;  // will contain the tag found
+    struct sdf_attribute*   a;  // will contain the attribute found
+	
+    // search the tag
+    e = sdf_element_search(elem, tag);
+
+    // die if not found (fatal error)
+    if(e == NULL)
+        print_and_die("Unable to find request tag.", -1);
+
+    a = sdf_attribute_search(e->attributes, name);
+
+    // die if not found (fatal error)
+    if(a == NULL)
+        print_and_die("Unable to find request attribute.", -1);
+    
+    // replace the string
+    sdf_replace_string(a->value, value);
+}
+
+/**
+ * Print the message and return the retval
+ * [IN] message: message to be terminal-printed
+ * [IN] retval: value to be returned to system
+ * [OUT] void
+ **/
+void build_world(struct sdf_document* world) {
+    // files list
+    int                 i;
+    struct sdf_file     files[NUM_FILES];
+    struct sdf_document documents[NUM_FILES];
+
+    // for each files, open file, create document and append it to world
+    for(i = 0; i < NUM_FILES; i++) {
+        sdf_file_open(&files[i], names[i]);
+        sdf_document_create(&files[i], &documents[i]);
+        sdf_element_append(&world->root->children, documents[i].root);
+    }
+}
+
+/**
+ * Build a box and add it to the world
+ * [IN] message: message to be terminal-printed
+ * [IN] retval: value to be returned to system
+ * [OUT] void
+ **/
+void add_box(struct sdf_document* world, int box_id, float x, float y, float z) {
+    struct sdf_file     box_f;
+    struct sdf_document box_d;
+    char                pose[MAX_POSE_LEN];
+    char                name[MAX_NAME_LEN];
+
+    // open file and parse it
+    sdf_file_open(&box_f, BOX_FILE);
+    sdf_document_create(&box_f, &box_d);
+
+    // clean buffer and sprintf new position and name
+    memset(name, 0, MAX_NAME_LEN * sizeof(char));
+    memset(pose, 0, MAX_POSE_LEN * sizeof(char));
+    sprintf(name, "'Box_Red_%d'", box_id);
+    sprintf(pose, "%.3f %.3f %.3f 0 0 0", x, y, z);
+
+    // substitute name and position in document and append it
+    search_n_replace_attr(box_d.root, "model", "name", name);
+    search_n_replace_cont(box_d.root->children, "pose", pose);
+    sdf_element_append(&world->root->children, box_d.root);
+}
+
+/**
+ * Generate a maze and print onto screen
+ * [IN] struct maze*: maze will be stored here
+ * [IN] char* w_str: must contain width in string version
+ * [IN] char* h_str: must contain height in string version
+ * [OUT] void
+ **/
+void generate_maze(struct maze* m, char* w_str, char* h_str) {	
+	int ret;    // check for return values
+    int width;  // will contains width in numeric shape
+    int height; // will contains height in numeric shape
+    
+    // get width from str
+    ret = sscanf(w_str, "%d", &width);
+	if (ret < 1)
+		print_and_die("Invalid size 1 provided.", -1);
+
+    // get height from str
+	ret = sscanf(h_str, "%d", &height);
+	if (ret < 1)
+		print_and_die("Invalid size 2 provided.", -1);
+
+    // width and height must be odd
+	if (!(width % 2) || !(height % 2))
+		print_and_die("Only odd size are valid.", -1);
+	
+    // width and height must be positive
+	if (width <= 0 || height <= 0)
+		print_and_die("Dimension must be positive.", -1);
+
+    // init the maze graph
+	ret = init_maze(m, width, height);
+
+    // exit in case of memory exhaust
+	if (ret != 0)
+		print_and_die("Out of memory.", -1);
+
+    // create the maze
+    create_maze(m);
+
+    // print onto screen
+    draw_maze(m);
+}
+
